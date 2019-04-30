b448e68b138cf38ddcbcf31574f5eef224e57a1c
thomas-mortensen <thomas.mortensen@hpe.com>
2019-04-30 15:39:55 +0200

    Align with SD version 2.7.2

--- docker/images/sd-aio/build.sh
+++ docker/images/sd-aio/build.sh
@@ -13,7 +13,7 @@ ISO_MOUNT_POINT=iso
 IMGNAME=sd-aio
 
 # SD version the image is based on
-SDVERSION=2.7.1
+SDVERSION=2.7.2
 
 # Base tag name
 BASETAG=${BASETAG:-latest}
--- docker/images/sd-ui/build.sh
+++ docker/images/sd-ui/build.sh
@@ -13,7 +13,7 @@ ISO_MOUNT_POINT=iso
 IMGNAME=sd-ui
 
 # SD version the image is based on
-SDVERSION=2.7.1
+SDVERSION=2.7.2
 
 # Base tag name
 BASETAG=${BASETAG:-latest}
